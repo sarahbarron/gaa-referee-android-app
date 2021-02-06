95c1b7092ca788d1b07ff2d6a297f436730adfd8
sarahbarron <sarahflavin@yahoo.com>
2021-02-06 23:37:29 +0000

    dependencies and drawables added

--- app/src/main/java/org/wit/gaa_referee_app/ui/home/loginActivity.kt
+++ app/src/main/java/org/wit/gaa_referee_app/ui/home/loginActivity.kt
@@ -1,11 +1,12 @@
-package org.wit.gaa_referee_app
+package org.wit.gaa_referee_app.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.wit.gaa_referee_app.R
 
-class MainActivity : AppCompatActivity() {
+class loginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
